84cb8172313f7fe8978373c36a38f288da4e504e
TarzaxDev <98848694+TarzaxDev@users.noreply.github.com>
2024-08-26 01:57:10 +0200

    error + success + info + debug + warn logs added ✨

--- main.go
+++ main.go
@@ -8,5 +8,29 @@ import (
 func main() {}
 
 func colorLog(colorMessage, message string, args ...any) {
-	fmt.Printf(colorMessage+message+color.Reset, args...)
+	if len(args) > 0 {
+		fmt.Printf(colorMessage+message+color.Reset, args...)
+	} else {
+		fmt.Print(colorMessage + message + color.Reset)
+	}
+}
+
+func errorLog(message string, args ...any) {
+	colorLog(color.Red, message, args...)
+}
+
+func successLog(message string, args ...any) {
+	colorLog(color.Green, message, args...)
+}
+
+func infoLog(message string, args ...any) {
+	colorLog(color.Blue, message, args...)
+}
+
+func debugLog(message string, args ...any) {
+	colorLog(color.Magenta, message, args...)
+}
+
+func warnLog(message string, args ...any) {
+	colorLog(color.Yellow, message, args...)
 }
