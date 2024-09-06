3316bd0996e0be3091feb17123ad4cd765bcc25d
TarzaxDev <98848694+TarzaxDev@users.noreply.github.com>
2024-09-07 00:44:32 +0200

    func colorLog => ColorLog ✨

--- color_function.go
+++ color_function.go
@@ -2,7 +2,7 @@ package GoColor
 
 import "fmt"
 
-func colorLog(colorMessage string, message string, useBackgroundColor bool, backgroundColor string, args ...any) {
+func ColorLog(colorMessage string, message string, useBackgroundColor bool, backgroundColor string, args ...any) {
 	if useBackgroundColor {
 		if len(args) > 0 {
 			fmt.Printf(backgroundColor+colorMessage+message+Reset, args...)
@@ -21,21 +21,21 @@ func colorLog(colorMessage string, message string, useBackgroundColor bool, back
 }
 
 func ErrorLog(message string, args ...interface{}) {
-	colorLog(LightRed, message, false, "", args...)
+	ColorLog(LightRed, message, false, "", args...)
 }
 
 func SuccessLog(message string, args ...interface{}) {
-	colorLog(LightGreen, message, false, "", args...)
+	ColorLog(LightGreen, message, false, "", args...)
 }
 
 func InfoLog(message string, args ...interface{}) {
-	colorLog(LightBlue, message, false, "", args...)
+	ColorLog(LightBlue, message, false, "", args...)
 }
 
 func DebugLog(message string, args ...interface{}) {
-	colorLog(LightMagenta, message, false, "", args...)
+	ColorLog(LightMagenta, message, false, "", args...)
 }
 
 func WarnLog(message string, args ...interface{}) {
-	colorLog(LightYellow, message, false, "", args...)
+	ColorLog(LightYellow, message, false, "", args...)
 }
